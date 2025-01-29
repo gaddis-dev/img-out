ea774b8f806f9b5cc46c7c430ea4a8b1fafc090c
gaddisgamer <139636962+gaddis-dev@users.noreply.github.com>
2025-01-29 13:30:15 +0000

    help

--- image.py
+++ image.py
@@ -3,4 +3,4 @@ import PIL.Image
 image = PIL.Image.open(input('image: ') + '.jpg')
 input()
 image = image.resize(size()[::-1])
-print(list(image.getdata()), image)
+print(list(image.getdata()), image, '\033[48;5;{r};{g};{}m h')
